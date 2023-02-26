64687c2ca99467c3985fe0cf6e5bc08519e174b7
Dan Hung <dan891221@gmail.com>
2023-02-27 01:15:07 +0800

    A nice animated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,40 @@
-import '../assets/Navbar.css'
+import "../assets/Navbar.css";
 
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 function Navbar() {
-  return (
-    <div className='navbar navbar__black'>
-      <div className="navbar__contents">
-      <img
-        className='navbar__logo'
-        src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
-        alt='netlfix-logo'
-      />
+  const [show, setShow] = useState(false);
 
-      <img
-        className='navbar__avatar'
-        src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
-        alt='avatar'
-      />
+  const transitionNavbar = () => {
+    if (window.scrollY > 100) {
+      setShow(true);
+    } else {
+      setShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavbar);
+    return () => window.removeEventListener('scroll', transitionNavbar)
+  }, []);
+
+  return (
+    <div className={`navbar ${show && "navbar__black"}`}>
+      <div className='navbar__contents'>
+        <img
+          className='navbar__logo'
+          src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
+          alt='netlfix-logo'
+        />
+
+        <img
+          className='navbar__avatar'
+          src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png'
+          alt='avatar'
+        />
       </div>
     </div>
   );
 }
 
-export default Navbar
+export default Navbar;
